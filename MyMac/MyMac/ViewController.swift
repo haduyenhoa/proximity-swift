0130fae509365188af87d837071a1d2960c774b2
Ha Duyen Hoa <tyt_g207@yahoo.com>
2015-04-01 23:58:01 +0200

    iOS application to use with Proximity on Mac.

---
+++ MyMac/MyMac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMac
+//
+//  Created by Duyen Hoa Ha on 01/04/2015.
+//  Copyright (c) 2015 Duyen Hoa Ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
